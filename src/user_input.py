d685b3db165c42b537dddc50dd73d3321b51c800
Nisha Chowdhury <nisha10cs30@gmail.com>
2025-12-16 15:50:13 +0530

    Fixed risk mapping to support C/M/A and full names

--- src/user_input.py
+++ src/user_input.py
@@ -4,12 +4,6 @@ class UserInput:
         self.risk = risk.lower()
         self.duration_months = duration_months
 
-    class UserInput:
-    def __init__(self, amount, risk, duration_months):
-        self.amount = amount
-        self.risk = risk.lower()
-        self.duration_months = duration_months
-
     def risk_factor(self):
         mapping = {
             "c": 0.7,
@@ -17,7 +11,7 @@ class UserInput:
             "m": 1.0,
             "moderate": 1.0,
             "a": 1.3,
-            "aggressive": 1.3
+            "aggressive": 1.3,
         }
         return mapping[self.risk]
 
